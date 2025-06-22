52ef2b794c1fc4abdf9d6f1f070e430b4eed76ea
Asad-code173 <asadfarzand711@gmail.com>
2025-06-22 05:44:15 -0700

    hiwebhooks

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+eslint: {
+    ignoreDuringBuilds: true, // ✅ This disables ESLint during production build
+  },
+ 
+
 };
 
 export default nextConfig;
